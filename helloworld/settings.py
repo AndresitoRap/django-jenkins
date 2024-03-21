96c2d2bd55227cf2df2452415a5d1a63b68f0657
AndresitoRap <andresfelipecvs@hotmail.com>
2024-03-20 21:13:01 -0500

    Update Settings.py

--- helloworld/settings.py
+++ helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-w&mfjdebo02p3u3$z#rbv^e2@ikrq^_xk5f-!3q)&v+!e0kf9j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.20.21.119']
+ALLOWED_HOSTS = ['172.20.21.119', 'localhost']
 
 
 # Application definition
